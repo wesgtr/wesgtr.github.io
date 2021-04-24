a593be3f855fa742b2c855771da85223555a695f
WesGatner <wesgtr@gmail.com>
2021-04-24 17:08:41 -0300

    finish project 01

--- js/main.js
+++ js/main.js
@@ -3,17 +3,12 @@ const links = [
         label: "Week1 Notes",
         url: "week1/index.html"
     },
-    {
-        label: "Week2 Notes",
-        url: "week2/index.html"
-    },
 ]
 
 window.onload = function(){
     let ul=document.createElement('ul');
     for(i=0;i<links.length;i++)
     {
-        console.log('i ',i);
         var li=document.createElement('li');
         li.innerHTML="<a onclick='redirectTo(i)'>" + links[i].label + "</a>";
         ul.appendChild(li);
@@ -22,5 +17,5 @@ window.onload = function(){
 };
 
 redirectTo = function (param) {
-    // window.location.href="week"+param+"/index.html";
+    window.location.href="week"+param+"/index.html";
 }
